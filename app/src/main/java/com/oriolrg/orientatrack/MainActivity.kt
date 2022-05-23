2e52cadd2195f0846a6a9c254244d6e0c4db6c9e
oriol <oriolrg@gmail.com>
2022-05-23 17:37:09 +0200

    neteja

--- app/src/main/java/com/oriolrg/orientatrack/MainActivity.kt
+++ app/src/main/java/com/oriolrg/orientatrack/MainActivity.kt
@@ -23,7 +23,10 @@ import java.lang.Math.*
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.parsers.ParserConfigurationException
 
-data class Point( var altura: String, var temps: String, var lat: String, var lon: String)
+data class Point( var lat: Double, var lon: Double){
+    var altura: Double? =0.0
+    var temps: String? = null
+}
 data class Track(var name: String, var punts: Point)
 class MainActivity : AppCompatActivity() {
     var trkDataHashMap = HashMap< String, String>()
@@ -31,10 +34,7 @@ class MainActivity : AppCompatActivity() {
     var mylist : MutableList<Track>? = null
     //Variables posicio temps real
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    //var iniciX:Double = 39.0099912
-    //var iniciY:Double = -94.581212
-    var destiX:Double = 42.13708
-    var destiY:Double = 1.58497
+    var desti = Point( 42.13708, 1.58497)
 
         //permision id es unic
     private var PERMISION_ID = 1000
@@ -43,26 +43,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         try {
             val lv = findViewById<ListView>(R.id.listView)
-            carregarTrack();
+            carregarTrack()
             fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
             Log.d("Debug:",CheckPermission().toString())
             Log.d("Debug:",isLocationEnabled().toString())
             ActivityResultContracts.RequestPermission()
-            /* fusedLocationProviderClient.lastLocation.addOnSuccessListener{location: Location? ->
-                    textView.text = location?.latitude.toString() + "," + location?.longitude.toString()
-            }*/
             Log.d("debug", "getLastLocation()")
             getLastLocation()
             NewLocationData()
-
-            //Mostra la llista de punts per pantalla
-            /*val adapter = SimpleAdapter(
-                this@MainActivity,
-                trkList,
-                R.layout.custom_list, arrayOf("nom", "altura", "temps", "coordenades"),
-                intArrayOf(R.id.nom, R.id.altura, R.id.temps, R.id.coordenades))
-            lv.setAdapter(adapter)*/
         } catch (e: IOException) {
             e.printStackTrace()
         } catch (e: ParserConfigurationException) {
@@ -86,26 +75,24 @@ class MainActivity : AppCompatActivity() {
                 RequestPermission()
             }else{
                 fusedLocationProviderClient.lastLocation.addOnCompleteListener { task->
-                    var location: Location? = task.result
+                    val location: Location? = task.result
                     if(location == null){
                         Log.d("Debug:" ,"NewLocationData")
                         val locationTipus = findViewById<TextView>(R.id.locationTipus)
                         locationTipus.text = "NewLocationData"
                         NewLocationData()
                     }else{
-
-                        val DistanciaTxt = findViewById<TextView>(R.id.DistanciaTxt)
-
-                        Log.d("Debug:" ,"Your Location:"+ location.longitude)
+                        var localitzacio = Point(location.latitude,location.longitude)
+                        val distanciaTxt = findViewById<TextView>(R.id.DistanciaTxt)
                         LocalitzacioTxt.text = "Les teves cordenades son: \n Latitud:" + location.latitude + "; Longitud:" + location.longitude + "; Altitud:" + location.altitude
 
                         //var distanciaPunt = calcularDistanciaEntreCoordenades(location.latitude,location.longitude, destiX, destiY)
                         //var anglePunt = calculateAngle(location.latitude,location.longitude, destiX, destiY)
-                        var distanciaPunt = calcularDistanciaEntreCoordenades(location.latitude,location.longitude, destiX, destiY)
-                        var anglePunt = calculateAngle(location.latitude,location.longitude, destiX, destiY)
+                        var distanciaPunt = calcularDistanciaEntreCoordenades(localitzacio.lat,localitzacio.lon, desti.lat, desti.lon)
+                        var anglePunt = calculateAngle(localitzacio.lat,localitzacio.lon, desti.lat, desti.lon)
                         val locationTipus = findViewById<TextView>(R.id.locationTipus)
                         locationTipus.text = anglePunt.toString()+"º"
-                        DistanciaTxt.text = "El punt està a:" + distanciaPunt + "m"
+                        distanciaTxt.text = "El punt està a:" + distanciaPunt + "m"
                     }
                 }
             }
@@ -115,10 +102,15 @@ class MainActivity : AppCompatActivity() {
     }
     fun NewLocationData(){
         Log.d("NewLocationData Debug:" ,"Entra")
-        var locationRequest =  LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        var locationRequest =  LocationRequest.create().apply {
+            interval = 100
+            fastestInterval = 50
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            maxWaitTime = 100
+        }
+        /*locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
+        locationRequest.fastestInterval = 0*/
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         if (ActivityCompat.checkSelfPermission(
@@ -142,33 +134,32 @@ class MainActivity : AppCompatActivity() {
         override fun onLocationResult(locationResult: LocationResult) {
             val LocalitzacioTxt = findViewById<TextView>(R.id.LocalitzacioTxt)
             val DistanciaTxt = findViewById<TextView>(R.id.DistanciaTxt)
-            var lastLocation = locationResult.lastLocation
+            var lastLocation = Point(locationResult.lastLocation.latitude,locationResult.lastLocation.longitude)
+            lastLocation.altura = locationResult.lastLocation.altitude
+            //var lastLocation2 = Location(lastLocation.latitude,lastLocation.longitude)
             //Assignem la nova localitzacio
 
-            var distanciaPunt = calcularDistanciaEntreCoordenades(lastLocation.latitude,lastLocation.longitude, destiX, destiY)
-            var anglePunt = calculateAngle(lastLocation.latitude,lastLocation.longitude, destiX, destiY)
+            var distanciaPunt = calcularDistanciaEntreCoordenades(lastLocation.lat,lastLocation.lon, desti.lat, desti.lon)
+            var anglePunt = calculateAngle(lastLocation.lat,lastLocation.lon, desti.lat, desti.lon)
             val locationTipus = findViewById<TextView>(R.id.locationTipus)
             locationTipus.text = anglePunt.toString()+"º"
             DistanciaTxt.text = "El punt està a:" + distanciaPunt + "m"
 
-            LocalitzacioTxt.text = "Les teves cordenades son: \n Latitud:" + lastLocation.latitude + "; Longitud:" + lastLocation.longitude + "; Altitud:" + lastLocation.altitude
+            LocalitzacioTxt.text = "Les teves cordenades son: \n Latitud:" + lastLocation.lat + "; Longitud:" + lastLocation.lon + "; Altitud:" + lastLocation.altura
 
         }
 
     }
     private fun calculateAngle(x1:Double, y1:Double, x2:Double, y2:Double):Double
     {
-        var distanciaX = findViewById<TextView>(R.id.distanciaX)
-        var distanciaY = findViewById<TextView>(R.id.distanciaY)
-
         var start_latitude  = toRadians(x1)
         var start_longitude = toRadians(y1)
         var stop_latitude   = toRadians(x2)
         var stop_longitude  = toRadians(y2)
-        var y = kotlin.math.sin(stop_longitude-start_longitude) * kotlin.math.cos(stop_latitude);
+        var y = kotlin.math.sin(stop_longitude-start_longitude) * kotlin.math.cos(stop_latitude)
         var x = kotlin.math.cos(start_latitude)*kotlin.math.sin(stop_latitude) -
-                kotlin.math.sin(start_latitude)*kotlin.math.cos(stop_latitude)*kotlin.math.cos(stop_longitude-start_longitude);
-        var brng = toDegrees(kotlin.math.atan2(y, x));
+                kotlin.math.sin(start_latitude)*kotlin.math.cos(stop_latitude)*kotlin.math.cos(stop_longitude-start_longitude)
+        var brng = toDegrees(kotlin.math.atan2(y, x))
         if (brng < 0 ) {
             brng = brng + 360
         }
@@ -220,9 +211,7 @@ class MainActivity : AppCompatActivity() {
         val docBuilder = builderFactory.newDocumentBuilder()
         val doc = docBuilder.parse(istream)
         val nList = doc.getElementsByTagName("trkpt")
-        val eTrack = doc.getElementsByTagName("metadata").item(0) as Element
-        val lenPoints = getPunts(nList)
-        var total: Double = 0.0
+        var total = 0.0
 
         for (i in 0 until nList.getLength()-1 step 1) {
             if (nList.item(0).getNodeType().equals(Node.ELEMENT_NODE)) {
@@ -240,13 +229,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calcularDistanciaEntreCoordenades(lat1:Double, lon1:Double,lat2:Double, lon2:Double): Double {
-        var R = 6378137;//Radi de la tierra en km
-        var dLat = toRadians(lat2) - toRadians(lat1);
-        var dLong = toRadians(lon2) - toRadians(lon1);
-        var a = sin(dLat / 2) * sin(dLat / 2) + cos(toRadians(lat1.toDouble())) * cos(toRadians(lat2.toDouble())) * sin(dLong / 2) * sin(dLong / 2);
-        var c = 2 * atan2(sqrt(a), sqrt(1 - a));
-        var d = R * c;
-        return d;//Retorna tres decimals
+        var R = 6378137//Radi de la tierra en km
+        var dLat = toRadians(lat2) - toRadians(lat1)
+        var dLong = toRadians(lon2) - toRadians(lon1)
+        var a = sin(dLat / 2) * sin(dLat / 2) + cos(toRadians(lat1)) * cos(toRadians(lat2)) * sin(dLong / 2) * sin(dLong / 2)
+        var c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        var d = R * c
+        return d//Retorna tres decimals
     }
 
 
@@ -255,7 +244,7 @@ class MainActivity : AppCompatActivity() {
     * Carrego el track de assets
     * TODO emmagatzemar els punts en una estructura de dades o Objecte que pugui treballar
     * */
-    public fun carregarTrack() {
+    fun carregarTrack() {
         val istream = assets.open("myGpx.gpx")
         val builderFactory = DocumentBuilderFactory.newInstance()
         val docBuilder = builderFactory.newDocumentBuilder()
@@ -273,18 +262,19 @@ class MainActivity : AppCompatActivity() {
                 trkDataHashMap = HashMap()
                 val element = nList.item(i) as Element
                 val track = Track(getNodeValue("name", eTrack),
-                    Point(getNodeValue("ele", element),
-                        getNodeValue("time", element),
-                        nList.item(i).attributes.getNamedItem("lat").nodeValue,
-                        nList.item(i).attributes.getNamedItem("lon").nodeValue))
+                    Point(
+                        nList.item(i).attributes.getNamedItem("lat").nodeValue.toDouble(),
+                        nList.item(i).attributes.getNamedItem("lon").nodeValue.toDouble()))
+                track.punts.altura = getNodeValue("ele", element).toDouble()
+                track.punts.temps = getNodeValue("time", element)
                 val (name, punt) = track
                 mylist?.add(track)
                 trkDataHashMap.put("nom", name)
-                trkDataHashMap.put("altura", punt.altura)
-                trkDataHashMap.put("temps", punt.temps)
-                trkDataHashMap.put("coordenades", "X: " + punt.lat + ", Y: " + punt.lon)
-                trkDataHashMap.put("lat", punt.lat)
-                trkDataHashMap.put("lon", punt.lon)
+                trkDataHashMap["altura"] = punt.altura.toString()
+                trkDataHashMap["temps"] = punt.temps.toString()
+                trkDataHashMap["coordenades"] = "X: " + punt.lat + ", Y: " + punt.lon
+                trkDataHashMap["lat"] = punt.lat.toString()
+                trkDataHashMap["lon"] = punt.lon.toString()
                 //adding the HashMap data to ArrayList
                 trkList.add(trkDataHashMap)
             }
@@ -309,7 +299,7 @@ class MainActivity : AppCompatActivity() {
             if (node.hasChildNodes()) {
                 val child = node.getFirstChild()
                 while (child != null) {
-                    if (child.getNodeType() === Node.TEXT_NODE) {
+                    if (child.getNodeType() == Node.TEXT_NODE) {
                         return child.getNodeValue()
                     }
                 }
